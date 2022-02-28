e414c4e2e588d034cad26ef4686aa98a85a95ead
joyjsmun <joy.js.mun@gmail.com>
2022-02-27 18:53:20 -0800

    ADD: coin info and price info

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -40,6 +40,17 @@ function Coin(){
     const [loading,setLoading] = useState(true);
     const {coinId} = useParams<RouteParams>();
     const {state} = useLocation<RouteState>();
+    const [info,setInfo] = useState();
+    const [priceInfo,setPrice] = useState();
+    useEffect(() => {
+        (async () => {
+            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json()
+            const priceData = await(await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+            setInfo(infoData);
+            setPrice(priceData);
+
+        })();
+    },[])
     
     return<Container>
     <Header>
