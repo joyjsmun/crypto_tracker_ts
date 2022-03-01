f06f5317f0da1c1c3d2fe0e503ea4c871f2d216a
joyjsmun <joy.js.mun@gmail.com>
2022-02-28 23:27:33 -0800

    ADD: Apexchart pt1

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,13 +1,70 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
 
-interface ICoin{
+interface ChartProps{
     coinId:string
 }
 
-function Chart({coinId}:ICoin){
-    const {isLoading,data} = useQuery(["ohlcv",coinId],() => fetchCoinHistory(coinId))
-    return <h1></h1>
+interface IHistorical{
+time_open: string;
+time_close: string;
+open: number;
+high:number;
+low: number;
+close: number;
+volume: number;
+market_cap: number;
+}
+
+function Chart({coinId}:ChartProps){
+    // 14 of them => array[]
+    const {isLoading,data} = useQuery<IHistorical[]>(["ohlcv",coinId],() => fetchCoinHistory(coinId));
+    return (
+        <div>
+        {isLoading ? 
+           ("Chart is loading..." 
+           ):(
+           <ApexChart 
+           type="line" 
+            series={[
+            {
+                name:"Price",
+                data:data?.map(price => price.close)
+            },
+            // {
+            //     name:"sales",
+            //     data:[1,2,3,4,5]
+            // }
+         ]}
+            options={{
+                theme:{
+                    mode:"dark"
+                },
+                chart:{
+                    height:300,
+                    width:500,
+                    toolbar:{
+                        show:false,
+                    },
+                    background: "transparent",
+                },
+                grid:{show:false},
+                stroke: {
+                    curve:"smooth",
+                    width: 4,
+                },
+                yaxis:{show:false},
+                xaxis:{
+                    axisBorder:{show:false},
+                    labels:{show:false},
+                axisTicks:{show:false},
+            },
+            }} 
+        />
+    )}
+    </div>
+    );
 }
 
 export default Chart;
