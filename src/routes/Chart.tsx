83b53b1103d93cdc7cff6b7c2d384d84e7d256f9
joyjsmun <joy.js.mun@gmail.com>
2022-03-02 21:15:56 -1000

    ADD: candle stick chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -30,28 +30,62 @@ function LineChart({coinId}:ChartProps){
            ("Chart is loading..." 
            ):(
            <ApexChart 
+           type="candlestick"
            series={[
-        
+            {
+                data:
+                [{
+                    x: new Date(1538778600000),
+                    y: [6629.81, 6650.5, 6623.04, 6633.33]
+                  },
+                  {
+                    x: new Date(1538780400000),
+                    y: [6632.01, 6643.59, 6620, 6630.11]
+                  },
+                  {
+                    x: new Date(1538782200000),
+                    y: [6630.71, 6648.95, 6623.34, 6635.65]
+                  },
+                  {
+                    x: new Date(1538784000000),
+                    y: [6635.65, 6651, 6629.67, 6638.24]
+                  },
+                  {
+                    x: new Date(1538785800000),
+                    y: [6638.24, 6640, 6620, 6624.47]
+                  }
+                ]}
            ]}
-           xaxis={{
-                type:'datetime'
-                }}
-
              options={{
+                theme:{
+                    mode:"dark"
+                },
                  chart:{
                      type:"candlestick",
                      height: 350,
+                     width:500,
+                     toolbar:{
+                         show:false,
+                     },
+                     background: "transparent",
                  },
                  yaxis:{
                      tooltip:{
-                         enabled:true
+                         enabled:false
                      }
-                 }
+                 },
+                 xaxis:{
+                     type:"datetime"
+                 },
+                 title: {
+                    text: "CandleStick Chart",
+                    align: "center",
+                  },
              }}
             
            />
-           )
-           }
+        )
+    }
         </div>
     )
 }
